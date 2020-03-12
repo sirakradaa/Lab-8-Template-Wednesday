3ba6149b80e046f0e8d9efdf3cbb6857d7125ee5
radaa <radaa@ualberta.ca>
2020-03-11 18:37:15 -0600

    test1 fail

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -93,4 +93,10 @@ class CityListTest {
 
         assertEquals(1, cityList.countCities());
     }
+
+    @Test
+    void testAddition() {
+        int test = 65;
+        assertEquals(4,test);
+    }
 }
